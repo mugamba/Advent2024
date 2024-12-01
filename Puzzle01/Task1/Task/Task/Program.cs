b35d24376c8c2506cb29ead2511a957ccc520211
Mate Sosa <mate.sosa@pakel.hr>
2024-12-01 07:15:06 +0100

    Advent of code puzzle one, Day 1: Historian Hysteria

---
+++ Puzzle01/Task1/Task/Task/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var lines = File.ReadAllLines("input.txt");
+var firstList = new List<int>();
+var secondList = new List<int>();
+var diffList = new List<long>();
+
+foreach (var line in lines)
+{
+    var splits = line.Split("   ");
+    firstList.Add(int.Parse(splits[0].Trim()));
+    secondList.Add(int.Parse(splits[1].Trim()));
+}
+
+firstList.Sort();
+secondList.Sort();
+
+for (int i = 0; i<firstList.Count; i++)
+{
+    /*Razlika se dodaje u apsolutnoj vrijednosti jer može bit i + i -, 
+     * ali distance je uvijek pozitivan*/
+    diffList.Add(Math.Abs(secondList[i] - firstList[i]));
+}
+
+/*Suma svih elemenata liste*/
+Console.WriteLine(diffList.Sum());
+Console.ReadKey();
---
+++ Puzzle01/Task2/Task/Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+var lines = File.ReadAllLines("input.txt");
+var firstList = new List<int>();
+var secondList = new List<int>();
+var simmList = new List<long>();
+
+foreach (var line in lines)
+{
+    var splits = line.Split("   ");
+    firstList.Add(int.Parse(splits[0].Trim()));
+    secondList.Add(int.Parse(splits[1].Trim()));
+}
+
+for (int i = 0; i < firstList.Count; i++)
+{
+    /*trenutni element*/
+    var selectedNumber = firstList[i];
+    /*filtriramo drugu listu sa odabranim elementom, 
+     * dobijemo broj ponavljanja u drugoj listi i množimo sa odabranim elem.*/
+    simmList.Add(secondList.Where(o=>o == firstList[i]).Count() * selectedNumber);
+}
+
+/*Suma dvih iz similarity liste*/
+Console.WriteLine(simmList.Sum());
+Console.ReadKey();
