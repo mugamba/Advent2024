7253b235a3b2fa57be7c09b411844cb0506ab405
Mate Sosa <mate.sosa@pakel.hr>
2024-12-03 07:09:40 +0100

    Avdent of code day 3

---
+++ Puzzle03/Task1/Task/Task/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var text = File.ReadAllText("input.txt");
+string pat = @"(mul\(\d+,\d+\))";
+RegexOptions options = RegexOptions.Multiline;
+var total = 0;
+foreach (Match m in Regex.Matches(text, pat, options))
+{
+    var newString =m.Value.Replace("mul(", "");
+    newString = newString.Replace(")", "");
+    var splits = newString.Split(",");
+    total+= int.Parse(splits[0]) * int.Parse(splits[1]);
+
+}
+
+
+
+/*Suma svih elemenata liste*/
+Console.WriteLine(total);
+Console.ReadKey();
---
+++ Puzzle03/Task2/Task/Task/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var text = File.ReadAllText("input.txt");
+string pat = @"(mul\(\d+,\d+\))";
+RegexOptions options = RegexOptions.Multiline;
+var dict = new Dictionary<int, int>();
+foreach (Match m in Regex.Matches(text, pat, options))
+{
+    var newString =m.Value.Replace("mul(", "");
+    newString = newString.Replace(")", "");
+    var splits = newString.Split(",");
+    dict.Add(m.Index, int.Parse(splits[0]) * int.Parse(splits[1]));
+}
+
+pat = @"(don't\(\).*do\(\))";
+var matches = Regex.Matches(text, pat, options);
+var dontRanges = new List<Tuple<int, int>>();
+foreach (Match m in matches)
+{
+    dontRanges.Add(new Tuple<int, int>(m.Index, m.Index + m.Length));
+}
+
+var filteredDict = dict.Where(x => !dontRanges.Any(o => o.Item1 < x.Key && o.Item2> x.Key)); 
+Console.WriteLine(filteredDict.Select(o=>o.Value).Sum());
+Console.ReadKey();
