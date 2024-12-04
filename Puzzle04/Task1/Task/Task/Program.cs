ecf2e56e91cfa726358568210c7d31a26cbca32f
Mate Sosa <mate.sosa@pakel.hr>
2024-12-04 08:45:10 +0100

    Day 4 advent of code

---
+++ Puzzle04/Task1/Task/Task/Program.cs
@@ -0,0 +1,293 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+
+internal class Program
+{
+    static char[,] _matrix;
+    static int _x;
+    static int _y;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var lines = File.ReadAllLines("input.txt");
+        _x = lines.Length;
+        _y = lines[0].Length;
+
+        _matrix = new char[_x, _y];
+
+        for (int i = 0; i < _x; i++)
+            for (int j = 0; j < _y; j++)
+                _matrix[i, j] = lines[i].ToCharArray()[j];
+
+
+        var counter = 0;
+        for (int i = 0; i < _x; i++)
+            for (int j = 0; j < _y; j++)
+            {
+                if (CheckLeft(i, j))
+                    counter++;
+                if (CheckRight(i, j))
+                    counter++;
+                if (CheckUp(i, j))
+                    counter++;
+                if (CheckDown(i, j))
+                    counter++;
+                if (CheckUpRight(i, j))
+                    counter++;
+                if (CheckUpLeft(i, j))
+                    counter++;
+                if (CheckDownRight(i, j))
+                    counter++;
+                if (CheckDownLeft(i, j))
+                    counter++;
+            }
+        /*Suma svih elemenata liste*/
+        Console.WriteLine(counter);
+        Console.ReadKey();
+    }
+
+    private static Boolean IsInsideMatrix(int i, int j)
+    {
+        if (i < 0 || j < 0)
+            return false;
+        if (i > _x - 1 || j > _y - 1)
+            return false;
+        return true;
+    }
+
+    private static Boolean CheckLeft(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i; nextIndexY = j + 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i;  nextIndexY = j + 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i;nextIndexY = j + 3;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+    }
+
+    private static Boolean CheckRight(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i; nextIndexY = j-1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i; nextIndexY = j-2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i; nextIndexY = j-3;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+    private static Boolean CheckUp(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i-1; nextIndexY = j;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i-2; nextIndexY = j;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i-3; nextIndexY = j;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+    private static Boolean CheckDown(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i + 1; nextIndexY = j;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i + 2; nextIndexY = j;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i + 3; nextIndexY = j;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+    private static Boolean CheckUpRight(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i - 1; nextIndexY = j+1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i - 2; nextIndexY = j + 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i - 3; nextIndexY = j + 3;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+    private static Boolean CheckUpLeft(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i - 1; nextIndexY = j - 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i - 2; nextIndexY = j - 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i - 3; nextIndexY = j - 3;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+    private static Boolean CheckDownLeft(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i + 1; nextIndexY = j - 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i + 2; nextIndexY = j - 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i + 3; nextIndexY = j - 3;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+    private static Boolean CheckDownRight(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'X')
+            return false;
+
+        nextIndexX = i + 1; nextIndexY = j + 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return false;
+
+        nextIndexX = i + 2; nextIndexY = j + 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return false;
+
+        nextIndexX = i + 3; nextIndexY = j + 3;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return false;
+        if (_matrix[nextIndexX, nextIndexY] != 'S')
+            return false;
+
+        return true;
+
+    }
+
+
+
+
+
+}
---
+++ Puzzle04/Task2/Task/Task/Program.cs
@@ -0,0 +1,165 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+
+internal class Program
+{
+    static char[,] _matrix;
+    static int _x;
+    static int _y;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var lines = File.ReadAllLines("input.txt");
+        _x = lines.Length;
+        _y = lines[0].Length;
+
+        _matrix = new char[_x, _y];
+
+        for (int i = 0; i < _x; i++)
+            for (int j = 0; j < _y; j++)
+                _matrix[i, j] = lines[i].ToCharArray()[j];
+
+
+        var counter = 0;List<Point> pointsOfA = new List<Point>();
+        for (int i = 0; i < _x; i++)
+            for (int j = 0; j < _y; j++)
+            {
+                var pointTemp = CheckUpRight(i, j);
+                if (pointTemp != null)
+                    pointsOfA.Add(pointTemp.Value);
+
+                pointTemp = CheckUpLeft(i, j);
+                if (pointTemp != null)
+                    pointsOfA.Add(pointTemp.Value);
+
+                pointTemp = CheckDownRight(i, j);
+                if (pointTemp != null)
+                    pointsOfA.Add(pointTemp.Value);
+
+
+                pointTemp = CheckDownLeft(i, j);
+                if (pointTemp != null)
+                    pointsOfA.Add(pointTemp.Value);
+
+            }
+
+        var count = pointsOfA.Distinct();
+
+        var test = pointsOfA.GroupBy(o => o).Select(g => new { A = g.Key, Broj = g.Count() }).ToList();
+        /*Suma svih elemenata liste*/
+        Console.WriteLine(test.Where(o=>o.Broj > 1).Count());
+        Console.ReadKey();
+    }
+
+    private static Boolean IsInsideMatrix(int i, int j)
+    {
+        if (i < 0 || j < 0)
+            return false;
+        if (i > _x - 1 || j > _y - 1)
+            return false;
+        return true;
+    }
+
+  
+   
+    private static Point? CheckUpRight(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'S')
+            return null;
+
+        nextIndexX = i - 1; nextIndexY = j+1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return null;
+
+        nextIndexX = i - 2; nextIndexY = j + 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return null;
+
+        return new Point(i - 1, j + 1);
+    
+    }
+
+    private static Point? CheckUpLeft(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'S')
+            return null;
+
+        nextIndexX = i - 1; nextIndexY = j - 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return null;
+
+        nextIndexX = i - 2; nextIndexY = j - 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return null;
+
+        return new Point(i-1, j-1);
+    }
+
+    private static Point? CheckDownLeft(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'S')
+            return null;
+
+        nextIndexX = i + 1; nextIndexY = j - 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return null;
+
+        nextIndexX = i + 2; nextIndexY = j - 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return null;
+
+        return new Point(i+1, j-1);
+
+    }
+
+    private static Point? CheckDownRight(int i, int j)
+    {
+        var nextIndexX = 0; var nextIndexY = 0;
+        if (_matrix[i, j] != 'S')
+            return null;
+
+        nextIndexX = i + 1; nextIndexY = j + 1;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'A')
+            return null;
+
+        nextIndexX = i + 2; nextIndexY = j + 2;
+        if (!IsInsideMatrix(nextIndexX, nextIndexY))
+            return null;
+        if (_matrix[nextIndexX, nextIndexY] != 'M')
+            return null;
+
+        return new Point(i+1, j+1);
+
+    }
+
+
+
+
+
+}
