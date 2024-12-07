cd304913bdca76d5f80833dde39558656061ec16
Mate Sosa <mate.sosa@pakel.hr>
2024-12-07 10:46:41 +0100

    Puzzle 07

---
+++ Puzzle06/Task1/Task/Task/Program.cs
@@ -0,0 +1,166 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+
+internal class Program
+{
+    static char[,] _matrix;
+    static int _x;
+    static int _y;
+    static Point _startPosition;
+    static List<Point> _path = new List<Point>();
+  
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("input.txt");
+        _x = lines.Length;
+        _y = lines[0].Length;
+
+        _matrix = new char[_x, _y];
+
+        for (int i = 0; i < _x; i++)
+            for (int j = 0; j < _y; j++)
+            {
+                var sign = lines[i].ToCharArray()[j];
+                _matrix[i, j] = sign;
+                if (sign == '^')
+                { 
+                    _startPosition = new Point(i, j);
+                }
+
+            }
+
+        AddPoints(_startPosition.X, _startPosition.Y);
+        /*Suma svih elemenata liste*/
+        var tt =  _path.Distinct().Count();
+        Console.ReadKey();
+    }
+
+    private static void AddPoints(int i, int j)
+    {
+        if (i < 0 || j < 0)
+            return;
+        if (i > _x - 1 || j > _y - 1)
+            return;
+
+        while (!(i < 0 || j < 0) && !(i > _x - 1 || j > _y - 1))
+        {
+            _path.Add(new Point(i, j));
+
+            if (_matrix[i, j] == '^')
+            {
+                if (!IsObsticle(i - 1, j))
+                {
+                    _matrix[i, j] = '.';
+                     i = i - 1;
+                    if (i >= 0 && i < _x)
+                        _matrix[i, j] = '^';
+                    continue;
+                }
+                else
+                {
+                    Rotate90(i, j);
+                    continue;
+                }
+            }
+            if (_matrix[i, j] == '>')
+            {
+                if (!IsObsticle(i, j + 1))
+                {
+                    _matrix[i, j] = '.';
+                    j = j + 1;
+                    if (j >= 0 && j < _y)
+                        _matrix[i, j] = '>';
+                    continue;
+                }
+                else
+                {
+                    Rotate90(i, j);
+                    continue;
+                }
+            }
+            if (_matrix[i, j] == 'v')
+            {
+                if (!IsObsticle(i + 1, j))
+                {
+                    _matrix[i, j] = '.';
+                    i = i + 1;
+                    if (i>=0 && i<_x)
+                        _matrix[i, j] = 'v';
+                    continue;
+                }
+                else
+                {
+                    Rotate90(i, j);
+                    continue;
+                }
+            }
+            if (_matrix[i, j] == '<')
+            {
+                if (!IsObsticle(i, j - 1))
+                {
+                    _matrix[i, j] = '.';
+                    j = j - 1;
+                    if (j >= 0 && j < _y)
+                        _matrix[i, j] = '<';
+          
+                    continue;
+                }
+                else
+                {
+                    Rotate90(i, j);
+                    continue;
+                }
+            }
+        }
+
+    }
+
+    private static Boolean IsObsticle(int i, int j)
+    {
+        if (i < 0 || j < 0)
+            return false;
+        if (i > _x - 1 || j > _y - 1)
+            return false;
+
+        return _matrix[i, j] == '#';
+    }
+
+    private static void Rotate90(int i, int j)
+    {
+        if (_matrix[i, j] == '^')
+        {
+            _matrix[i, j] = '>';
+            return;
+        }
+        if (_matrix[i, j] == '>')
+        {
+            _matrix[i, j] = 'v';
+            return;
+        }
+        if (_matrix[i, j] == 'v')
+        {
+            _matrix[i, j] = '<';
+            return;
+        }
+        if (_matrix[i, j] == '<')
+        {
+            _matrix[i, j] = '^';
+            return;
+        }
+
+        
+    }
+    
+
+
+
+
+
+}
---
+++ Puzzle07/Task2/Task/Task/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+
+internal class Program
+{
+    public static List<Tuple<long, List<long>>> _input = new List<Tuple<long, List<long>>>();
+
+
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("input.txt");
+        
+        foreach (var line in lines)
+        {
+            var splits = line.Split(':');
+            var key = long.Parse(splits[0].Trim());
+            var list = new List<long>();
+
+            var values = splits[1].Trim().Split(' ');
+            list.AddRange(values.Select(o => long.Parse(o)));
+            _input.Add(Tuple.Create(key, list));
+
+        }
+
+        long sum = 0;
+        foreach (var line in _input)
+        {
+            if (TestLine(line.Item1, line.Item2, 0, line.Item2[0]))
+                sum = sum + line.Item1;
+        }
+      
+
+        Console.WriteLine(sum);
+        Console.ReadKey();
+    }
+
+    private static Boolean TestLine(long key, List<long> elements, int index, long currentelement)
+    {
+        if (elements.Count - 1 == index  ) 
+            return key == currentelement;
+
+        return TestLine(key, elements, index + 1, currentelement * elements[index + 1])
+            || TestLine(key, elements, index + 1, currentelement + elements[index + 1])
+            || TestLine(key, elements, index + 1, long.Parse(currentelement.ToString() + elements[index + 1].ToString()));
+
+    }
+
+}
