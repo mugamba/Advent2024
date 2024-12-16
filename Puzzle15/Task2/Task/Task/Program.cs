dd266c91e72b1f9170245a6c05554a6510ec0b55
Mate Sosa <mate.sosa@pakel.hr>
2024-12-16 20:34:25 +0100

    Task2 puzzle 15

--- Puzzle15/Task2/Task/Task/Program.cs
+++ Puzzle15/Task2/Task/Task/Program.cs
@@ -9,6 +9,7 @@ using System.Runtime.CompilerServices;
 using System.Security;
 using System.Text;
 using System.Text.RegularExpressions;
+using static Program;
 
 
 internal class Program
@@ -82,18 +83,20 @@ internal class Program
         foreach (var c in builder.ToString().ToCharArray())
             _moves.Enqueue(c);
 
-        MoveLeft(new Point(_currentPosition.X, _currentPosition.Y));
 
-        // printmap(_x, _y);
 
-        //while (_moves.Count > 0)
-        //{
-        //  //  DoMove(_currentPosition);
-        //    //printmap(_x, _y);
-        //    //Console.WriteLine();
-        //}
 
-        //Console.WriteLine(sumstones(_x, _y));
+
+      
+        while (_moves.Count > 0)
+        {
+           
+            DoMove(_currentPosition);
+            //printmap(_x, _y);
+            //Console.WriteLine();
+        }
+
+        Console.WriteLine(_stones.Select(o=>o.One).Select(o=>o.X * 100 + o.Y).Sum());
         Console.ReadKey();
     }
 
@@ -103,71 +106,214 @@ internal class Program
         public Point Two { get; set; }
     }
 
-
-    private static Boolean IsObsticle(int i, int j)
+    private static void DoMove(Point p)
     {
-        if (_matrix[i, j] == '.')
-            return false;
-        else
-            return true;
+
+        if (_moves.TryDequeue(out var move))
+        {
+            if (move == '^')
+            {
+                MoveUp(p);
+            }
+            if (move == 'v')
+            {
+                MoveDown(p);
+            }
+            if (move == '<')
+            {
+                MoveLeft(p);
+            }
+            if (move == '>')
+            {
+                MoveRight(p);
+            }
+        }
     }
 
-    private static Boolean IsStone(int i, int j)
+    public static void MoveUp(Point currentPos)
     {
-        if (_matrix[i, j] == 'O')
-            return true;
-        else
-            return false;
+        var up = new Point(currentPos.X-1, currentPos.Y);
+
+        if (_blockades.Contains(up))
+            return;
+
+        var oldStones = new List<Stone>();
+        var counter = 0;
+        var toMakeMove = false;
+        var topPoints = new List<Point>() { up};
+        while (!toMakeMove)
+        {
+            
+            var listOfTopStone = _stones.Where(o => topPoints.Contains(o.One) || topPoints.Contains(o.Two)).ToList();
+            topPoints = listOfTopStone.Select(o => o.One).ToList();
+            topPoints = topPoints.Union(listOfTopStone.Select(o => o.Two)).ToList();
+            topPoints= topPoints.Select(o => new Point(o.X - 1, o.Y)).ToList();
+
+            if (topPoints.Any(o=>_blockades.Contains(o)))
+            {
+                return;
+            }
+          
+            if (listOfTopStone.Count == 0)
+            {
+                toMakeMove = true;
+            }
+            oldStones = oldStones.Union(listOfTopStone).ToList();
+            counter++;
+        }
+
+        if (toMakeMove)
+        {
+            foreach (var item in oldStones)
+            {
+                item.One = new Point(item.One.X-1, item.One.Y);
+                item.Two = new Point(item.Two.X-1, item.Two.Y);
+            }
+            _currentPosition = up;
+        }
+
     }
 
-    //private static void DoMove(Point p)
-    //{
-
-    //    if (_moves.TryDequeue(out var move))
-    //    {
-    //        if (move == '^')
-    //        {
-    //            MoveUp(p);
-    //        }
-    //        if (move == 'v')
-    //        {
-    //            MoveDown(p);
-    //        }
-    //        if (move == '<')
-    //        {
-    //            MoveLeft(p);
-    //        }
-    //        if (move == '>')
-    //        {
-    //            MoveRight(p);
-    //        }
-    //    }
-    //}
-
-    public static void MoveLeft(Point currentPoint)
+    public static void MoveDown(Point currentPos)
     {
-       var toMove = LeftStonesShouldMove(new List<Stone>(), new List<Point> { new Point(currentPoint.X, currentPoint.Y-1) });
+        var down = new Point(currentPos.X + 1, currentPos.Y);
+
+        if (_blockades.Contains(down))
+            return;
+
+        var oldStones = new List<Stone>();
+        var counter = 0;
+        var toMakeMove = false;
+        var topPoints = new List<Point>() { down };
+        while (!toMakeMove)
+        {
+            var listOfTopStone = _stones.Where(o => topPoints.Contains(o.One) || topPoints.Contains(o.Two)).ToList();
+            topPoints = listOfTopStone.Select(o => o.One).ToList();
+            topPoints = topPoints.Union(listOfTopStone.Select(o => o.Two)).ToList();
+            topPoints = topPoints.Select(o => new Point(o.X + 1, o.Y)).ToList();
+
+            if (topPoints.Any(o => _blockades.Contains(o)))
+            {
+                return;
+            }
+
+            if (listOfTopStone.Count == 0)
+            {
+                toMakeMove = true;
+            }
+            oldStones = oldStones.Union(listOfTopStone).ToList();
+            counter++;
+        }
+
+        if (toMakeMove)
+        {
+            foreach (var item in oldStones)
+            {
+                item.One = new Point(item.One.X + 1, item.One.Y);
+                item.Two = new Point(item.Two.X + 1, item.Two.Y);
+            }
+            _currentPosition = down;
+        }
 
     }
 
-    public static List<Stone> LeftStonesShouldMove(List<Stone> stones, List<Point> shouldMove)
-    {
-        /*Canot move ran into block*/
-        if (shouldMove.Any(o => _blockades.Contains(o)))
-            return null;
 
-        var list = _stones.Where(o => shouldMove.Contains(o.One) || shouldMove.Contains(o.Two)).ToList();
-        
+
+    public static void MoveLeft(Point currentPos)
+    {
+        var left = new Point(currentPos.X, currentPos.Y - 1);
+
+        if (_blockades.Contains(left))
+            return;
+
+        var list = _stones.Where(o => o.Two == left).ToList();
 
         if (list.Count == 0)
-            return stones;
+        {
+            _currentPosition = left;
+            return;
+        }
 
-        stones.AddRange(list);
-        var listNextMove = list.Select(o => new Point(o.One.X, o.One.Y - 1)).Union(list.Select(o => new Point(o.Two.X, o.Two.Y - 1))).ToList();
+        var counter = 0;
+        var toMakeMove = false;
+        while (!toMakeMove)
+        {
+            counter = counter + 2;
 
-        return LeftStonesShouldMove(stones, listNextMove);
+            if (_blockades.Contains(new Point(left.X, left.Y - counter)))
+            {
+                return;
+            }
+
+            var list1 = _stones.Where(o => o.Two == new Point(left.X, left.Y - counter)).ToList();
+            if (list1.Count == 0)
+            {
+                toMakeMove = true;
+
+               
+            }
+            list = list.Union(list1).ToList();
+        }
+
+        if (toMakeMove)
+        { 
+            foreach(var item in list) 
+            {
+                item.One = new Point(item.One.X, item.One.Y - 1);
+                item.Two = new Point(item.Two.X, item.Two.Y - 1);
+            }
+            _currentPosition = left;
+        }
 
     }
+
+    public static void MoveRight(Point currentPos)
+    {
+        var right = new Point(currentPos.X, currentPos.Y + 1);
+
+        if (_blockades.Contains(right))
+            return;
+
+        var list = _stones.Where(o => o.One == right).ToList();
+
+        if (list.Count == 0)
+        {
+            _currentPosition = right;
+            return;
+        }
+
+        var counter = 0;
+        var toMakeMove = false;
+        while (!toMakeMove)
+        {
+            counter = counter + 2;
+
+            if (_blockades.Contains(new Point(right.X, right.Y + counter)))
+            {
+                return;
+            }
+
+            var list1 = _stones.Where(o => o.One == new Point(right.X, right.Y + counter)).ToList();
+            if (list1.Count == 0)
+            {
+                toMakeMove = true;
+            }
+            list = list.Union(list1).ToList();
+        }
+
+        if (toMakeMove)
+        {
+            foreach (var item in list)
+            {
+                item.One = new Point(item.One.X, item.One.Y + 1);
+                item.Two = new Point(item.Two.X, item.Two.Y + 1);
+            }
+            _currentPosition = right;
+        }
+
+    }
+
+
 
 
 
@@ -178,30 +324,40 @@ internal class Program
         {
             for (int j = 0; j < y; j++)
             {
-                Console.Write(_matrix[i, j]);
+
+
+                if (_blockades.Contains(new Point(i, j)))
+                    Console.Write("#");
+                else
+                {
+                    if (_stones.Any(o => o.One == new Point(i, j)))
+                    {
+                        Console.Write("[");
+                    }
+                    else
+                    {
+                        if (_stones.Any(o => o.Two == new Point(i, j)))
+                            Console.Write("]");
+                        else
+                              if (_currentPosition == new Point(i, j))
+                            Console.Write("@");
+                        else
+                            Console.Write(".");
+                    }
+
+                }
+
+
+
             }
             Console.WriteLine();
         }
-    }
 
-    public static long sumstones(int x, int y)
-    {
-        var sum = 0L;
-        for (int i = 0; i < y; i++)
-        {
-            for (int j = 0; j < x; j++)
-            {
-                if (_matrix[i, j] == 'O')
-                    sum = sum + 100 * i + j;
-            }
 
-        }
-
-        return sum;
 
     }
 
-
+   
 
 
 
