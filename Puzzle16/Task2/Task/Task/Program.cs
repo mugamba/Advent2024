929cae24bbfbb4894e3e2fb9117030c2d4fcc0e8
Mate Sosa <mate.sosa@pakel.hr>
2024-12-17 15:11:07 +0100

    Puzzle 16, 17

--- Puzzle16/Task2/Task/Task/Program.cs
+++ Puzzle16/Task2/Task/Task/Program.cs
@@ -17,8 +17,7 @@ internal class Program
     static char[,] _matrix;
     static int _x;
     static int _y;
-    static Dictionary<Tuple<Point, char>, Tuple<Tuple<Point, char>, int>> _distances = new Dictionary<Tuple<Point, char>, Tuple<Tuple<Point, char>, int>>();
-    static Dictionary<Tuple<Point, char>, Tuple<Tuple<Point, char>, int>> _alternatePath = new Dictionary<Tuple<Point, char>, Tuple<Tuple<Point, char>, int>>();
+    static Dictionary<Tuple<Point, char>, Node> _distances = new Dictionary<Tuple<Point, char>, Node>();
     static Dictionary<Tuple<Point, char>, Boolean> _visited = new Dictionary<Tuple<Point, char>, Boolean>();
     static Tuple<Point, char> _start;
     static Point _end;
@@ -63,19 +62,19 @@ internal class Program
                     {
                         if (k == 0)
                         {
-                            _distances.Add(Tuple.Create(new Point(i, j), '>'), Tuple.Create<Tuple<Point, char>, int>(null, int.MaxValue));
+                            _distances.Add(Tuple.Create(new Point(i, j), '>'), new Node());
                         }
                         if (k == 1)
                         {
-                            _distances.Add(Tuple.Create(new Point(i, j), '^'), Tuple.Create<Tuple<Point, char>, int>(null, int.MaxValue));
+                            _distances.Add(Tuple.Create(new Point(i, j), '^'), new Node());
                         }
                         if (k == 2)
                         {
-                            _distances.Add(Tuple.Create(new Point(i, j), 'v'), Tuple.Create<Tuple<Point, char>, int>(null, int.MaxValue));
+                            _distances.Add(Tuple.Create(new Point(i, j), 'v'), new Node());
                         }
                         if (k == 3)
                         {
-                            _distances.Add(Tuple.Create(new Point(i, j), '<'), Tuple.Create<Tuple<Point, char>, int>(null, int.MaxValue));
+                            _distances.Add(Tuple.Create(new Point(i, j), '<'), new Node());
                         }
 
                     }
@@ -83,94 +82,86 @@ internal class Program
 
             }
 
-        _distances[_start] = Tuple.Create<Tuple<Point, char>, int>(null, 0);
+        var startNode = new Node();
+        startNode._distance = 0;
+        _distances[_start] = startNode;
 
         var endNode = false;
 
         while (!endNode)
         {
 
-            var toVisit = _distances.Where(o => !_visited.ContainsKey(o.Key)).OrderBy(o => o.Value.Item2).Take(1).FirstOrDefault();
+            var toVisit = _distances.Where(o => !_visited.ContainsKey(o.Key)).OrderBy(o => o.Value._distance).Take(1).FirstOrDefault();
             VisitNode(toVisit.Key);
-            endNode = _distances.Where(o => o.Key.Item1 == _end).Any(o => o.Value.Item2 != int.MaxValue);
+            endNode = _distances.Where(o => o.Key.Item1 == _end).Any(o => o.Value._distance != int.MaxValue);
 
         }
 
 
-        var result = _distances.Where(o => o.Key.Item1 == _end).OrderBy(o => o.Value.Item2).FirstOrDefault();
+        var result = _distances.Where(o => o.Key.Item1 == _end).OrderBy(o => o.Value._distance).FirstOrDefault();
 
         var next = result.Key;
-        _shortestPath.Add(next, _distances[next].Item2);
-        /*retrace steps*/
-        while (next != null)
-        {
-            if (_distances[next].Item2 == 0)
-            {
-                break;
-            }
+        _shortestPath.Add(next, 0);
 
-            var previous = _distances[next].Item1;
-            _shortestPath.Add(previous, _distances[previous].Item2);
-            next = previous;
+        var list = _distances[next]._list;
+
+        while (list.Count > 0)
+        {
+            var newList = new List<Tuple<Point, char>>();
+            foreach (var item in list)
+            {
+                if (!_shortestPath.ContainsKey(item))
+                    _shortestPath.Add(item, 0);
+                newList.AddRange(_distances[item]._list);
+            }
+            list = newList;
         }
 
-        var sameKeys = _shortestPath.Keys.Intersect(_alternatePath.Keys).ToList();
-       
-
-        foreach (var k in sameKeys)
-        {
-            var previous = _alternatePath[k].Item1;
-            if (!_shortestPath.ContainsKey(previous))
-            {
-                var next11 = previous;
-                /*retrace steps*/
-                while (next11 != null)
-                {
-                    if (_shortestPath.ContainsKey(next11))
-                    {
-                        break;
-                    }
-
-                    previous = _distances[next11].Item1;
-                    _shortestPath.Add(previous, _distances[previous].Item2);
-                    next11 = previous;
-                }
-
-            }
-
-        }
-
-
-        var shortestPathPoints = _shortestPath.Select(o => o.Key.Item1).Distinct().ToList();
-        Console.WriteLine(result.Value);
+        Console.WriteLine(_shortestPath.Select(o => o.Key.Item1).Distinct().Count());
         Console.ReadKey();
     }
 
 
     public static void VisitNode(Tuple<Point, char> pos)
     {
-        var currrentdistance = _distances[pos].Item2;
+        var currrentdistance = _distances[pos]._distance;
 
         if (pos.Item2 == '>')
         {
 
             if (_matrix[pos.Item1.X, pos.Item1.Y + 1] == '.')
             {
-                if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y + 1), '>')].Item2 > currrentdistance + 1)
-                    _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y + 1), '>')] = Tuple.Create(pos, currrentdistance + 1);
-                if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y + 1), '>')].Item2 == currrentdistance + 1)
-                    _alternatePath.Add(Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y + 1), '>'), Tuple.Create(pos, currrentdistance + 1));
+                var node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y + 1), '>')];
+                if (node._distance > currrentdistance + 1)
+                {
+                    node._list.Clear();
+                    node._list.Add(pos);
+                    node._distance = currrentdistance + 1;
+                }
+                if (node._distance == currrentdistance + 1)
+                {
+                    if (!node._list.Contains(pos))
+                        node._list.Add(pos);
+                }
             }
-                
+
         }
         if (pos.Item2 == '<')
         {
             if (_matrix[pos.Item1.X, pos.Item1.Y - 1] == '.')
             {
-                if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y - 1), '<')].Item2 > currrentdistance + 1)
-                    _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y - 1), '<')] = Tuple.Create(pos, currrentdistance + 1);
-                if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y -1), '<')].Item2 == currrentdistance + 1)
-                    _alternatePath.Add(Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y - 1), '<'), Tuple.Create(pos, currrentdistance + 1));
+                var node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y - 1), '<')];
+                if (node._distance > currrentdistance + 1)
+                {
+                    node._list.Clear();
+                    node._list.Add(pos);
+                    node._distance = currrentdistance + 1;
+                }
+                if (node._distance == currrentdistance + 1)
+                {
+                    if (!node._list.Contains(pos))
+                        node._list.Add(pos);
+                }
             }
 
         }
@@ -178,10 +169,20 @@ internal class Program
         {
             if (_matrix[pos.Item1.X - 1, pos.Item1.Y] == '.')
             {
-                if (_distances[Tuple.Create(new Point(pos.Item1.X - 1, pos.Item1.Y), '^')].Item2 > currrentdistance + 1)
-                    _distances[Tuple.Create(new Point(pos.Item1.X - 1, pos.Item1.Y), '^')] = Tuple.Create(pos, currrentdistance + 1);
-                if (_distances[Tuple.Create(new Point(pos.Item1.X - 1, pos.Item1.Y), '^')].Item2 == currrentdistance + 1)
-                    _alternatePath[Tuple.Create(new Point(pos.Item1.X - 1, pos.Item1.Y), '^')] = Tuple.Create(pos, currrentdistance + 1);
+
+                var node = _distances[Tuple.Create(new Point(pos.Item1.X - 1, pos.Item1.Y), '^')];
+                if (node._distance > currrentdistance + 1)
+                {
+                    node._list.Clear();
+                    node._list.Add(pos);
+                    node._distance = currrentdistance + 1;
+                }
+                if (node._distance == currrentdistance + 1)
+                {
+                    if (!node._list.Contains(pos))
+                        node._list.Add(pos);
+                }
+
             }
 
         }
@@ -189,52 +190,190 @@ internal class Program
         {
             if (_matrix[pos.Item1.X + 1, pos.Item1.Y] == '.')
             {
-                if (_distances[Tuple.Create(new Point(pos.Item1.X + 1, pos.Item1.Y), 'v')].Item2 > currrentdistance + 1)
-                    _distances[Tuple.Create(new Point(pos.Item1.X + 1, pos.Item1.Y), 'v')] = Tuple.Create(pos, currrentdistance + 1);
-                if (_distances[Tuple.Create(new Point(pos.Item1.X + 1, pos.Item1.Y), 'v')].Item2 == currrentdistance + 1)
-                    _alternatePath[Tuple.Create(new Point(pos.Item1.X + 1, pos.Item1.Y), 'v')] = Tuple.Create(pos, currrentdistance + 1);
-
+                var node = _distances[Tuple.Create(new Point(pos.Item1.X + 1, pos.Item1.Y), 'v')];
+                if (node._distance > currrentdistance + 1)
+                {
+                    node._list.Clear();
+                    node._list.Add(pos);
+                    node._distance = currrentdistance + 1;
+                }
+                if (node._distance == currrentdistance + 1)
+                {
+                    if (!node._list.Contains(pos))
+                        node._list.Add(pos);
+                }
             }
         }
 
         if (pos.Item2 == '>')
         {
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')].Item2 > currrentdistance + 2000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')] = Tuple.Create(pos, currrentdistance + 2000);
+            var node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')];
+            if (node._distance > currrentdistance + 2000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 2000;
+            }
+            if (node._distance == currrentdistance + 2000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
         }
 
         if (pos.Item2 == '<')
         {
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')].Item2 > currrentdistance + 2000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')] = Tuple.Create(pos, currrentdistance + 2000);
+            var node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')];
+            if (node._distance > currrentdistance + 2000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 2000;
+            }
+            if (node._distance == currrentdistance + 2000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
         }
 
 
         if (pos.Item2 == '^')
         {
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')].Item2 > currrentdistance + 2000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')] = Tuple.Create(pos, currrentdistance + 2000);
+            var node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), 'v')];
+            if (node._distance > currrentdistance + 2000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 2000;
+            }
+            if (node._distance == currrentdistance + 2000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
         }
         if (pos.Item2 == 'v')
         {
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')].Item2 > currrentdistance + 1000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')] = Tuple.Create(pos, currrentdistance + 1000);
-            if (_distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')].Item2 > currrentdistance + 2000)
-                _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')] = Tuple.Create(pos, currrentdistance + 2000);
+            var node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '<')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '>')];
+            if (node._distance > currrentdistance + 1000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 1000;
+            }
+            if (node._distance == currrentdistance + 1000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
+
+            node = _distances[Tuple.Create(new Point(pos.Item1.X, pos.Item1.Y), '^')];
+            if (node._distance > currrentdistance + 2000)
+            {
+                node._list.Clear();
+                node._list.Add(pos);
+                node._distance = currrentdistance + 2000;
+            }
+            if (node._distance == currrentdistance + 2000)
+            {
+                if (!node._list.Contains(pos))
+                    node._list.Add(pos);
+            }
         }
 
         _visited.Add(pos, true);
@@ -264,11 +403,17 @@ internal class Program
                 if (_matrix[i, j] == 'O')
                     sum = sum + 100 * i + j;
             }
-           
+
         }
 
         return sum;
 
+    }
+
+    public class Node
+    {
+        public List<Tuple<Point, char>> _list = new List<Tuple<Point, char>>();
+        public int _distance = int.MaxValue;
     }
 
 
---
+++ Puzzle17/Task1/Task/Task/Program.cs
@@ -0,0 +1,145 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization;
+
+
+internal class Program
+{
+    public static int RegisterA;
+    public static int RegisterB;
+    public static int RegisterC;
+    public static List<int> _instructions = new List<int>();
+    public static List<int> _output = new List<int>();
+
+
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("input.txt");
+        
+        foreach (var line in lines) {
+
+            if (string.IsNullOrEmpty(line)) continue;
+
+            if (line.Contains("Register A:"))
+                RegisterA = int.Parse(line.Replace("Register A:", "").Trim());
+            if (line.Contains("Register B:"))
+                RegisterB = int.Parse(line.Replace("Register B:", "").Trim());
+            if (line.Contains("Register C:"))
+                RegisterC = int.Parse(line.Replace("Register C:", "").Trim());
+
+            if (line.Contains("Program:"))
+            {
+               var trimedLine = line.Replace("Program:", "").Trim();
+               var splits = trimedLine.Split(",");
+
+                foreach(var split in  splits)
+                {
+                    _instructions.Add(int.Parse(split));
+                }
+
+                
+            }
+        }
+
+        var inPoint = 0;
+
+        while (inPoint < _instructions.Count)
+        {
+            var instruction = _instructions[inPoint];
+            inPoint++;
+            var operand = _instructions[inPoint];
+            inPoint++;
+            if (instruction == 3)
+                if (RegisterA != 0)
+                    inPoint = operand;
+                else
+                {
+                    continue;
+                }
+            else
+            {
+                GetOperation(instruction, operand);
+            }
+
+
+        }
+
+
+
+        Console.WriteLine (String.Join(",", _output));
+        Console.ReadKey();
+    }
+
+
+    public static Int32 ComboOperand(int i)
+    {
+        switch (i)
+        { 
+            case 0: return 0;
+            case 1: return 1;
+            case 2: return 2;
+            case 3: return 3;
+            case 4: return RegisterA;
+            case 5: return RegisterB;
+            case 6: return RegisterC;
+        }
+
+       throw new  Exception("Unknown operand :" + i.ToString());
+    
+    }
+
+    public static void GetOperation(int instruction, int operand)
+    {
+        switch (instruction)
+        {
+            case 0:
+                {
+                    var number = ComboOperand(operand);
+                    RegisterA = RegisterA /(int)(Math.Pow(2, number)); 
+                    break;
+                }
+            case 1: {
+                    RegisterB = RegisterB ^ operand;
+                    break;
+                };
+            case 2:
+                {
+                    var number = ComboOperand(operand);
+                    RegisterB = number % 8;
+                    break;
+                }
+            case 4:
+                {
+                    RegisterB = RegisterB ^ RegisterC;
+                    break;
+                };
+            case 5:
+                {
+                    var number = ComboOperand(operand);
+                    _output.Add(number % 8);
+                    break;
+                };
+            case 6:
+                {
+                    var number = ComboOperand(operand);
+                    RegisterB = RegisterA / (int)(Math.Pow(2, number));
+                    break;
+                }
+            case 7:
+                {
+                    var number = ComboOperand(operand);
+                    RegisterC = RegisterA / (int)(Math.Pow(2, number));
+                    break;
+                }
+
+        }
+
+     
+
+    }
+
+}
