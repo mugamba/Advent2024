017549d26fcdb0cc20cf9c2a4f41678c4d79358d
Mate Sosa <mate.sosa@pakel.hr>
2024-12-23 10:49:57 +0100

    Task1 2023

--- Puzzle23/Task1/Task/Task/Program.cs
+++ Puzzle23/Task1/Task/Task/Program.cs
@@ -12,7 +12,7 @@ using System.Runtime.Serialization;
 internal class Program
 {
     public static Dictionary<string, List<string>> _keyValuePairs = new Dictionary<string, List<string>>();
-    public static List<Tuple<string, string, string>> _threeConnected = new List<Tuple<string, string, string>>();
+    public static string _wifi;
 
     static void Main(string[] args)
     {
@@ -23,53 +23,36 @@ internal class Program
             if (_keyValuePairs.ContainsKey(split[0]))
                 _keyValuePairs[split[0]].Add(split[1]);
             else
-                _keyValuePairs.Add(split[0], new List<string>() { split[1] });
+                _keyValuePairs.Add(split[0], new List<string>() { split[1], split[0] });
 
             if (_keyValuePairs.ContainsKey(split[1]))
                 _keyValuePairs[split[1]].Add(split[0]);
             else
-                _keyValuePairs.Add(split[1], new List<string>() { split[0] });
-
-
+                _keyValuePairs.Add(split[1], new List<string>() { split[0], split[1] });
         }
-
+        var totalMax = 0;
         foreach (var k in _keyValuePairs)
         {
+            var list = new List<string>(); 
             foreach (var el in k.Value)
             {
-                 var intersect = k.Value.Intersect(_keyValuePairs[el]).ToList();
-
-                if (intersect.Any())
-                    foreach (var v in intersect)
-                    { 
-                            var list = new List<string>();
-                        list.Add(k.Key);
-                        list.Add(v);
-                        list.Add(el);
-
-                        var toList = list.Order().ToArray();
-
-                        var tuple = Tuple.Create(toList[0], toList[1], toList[2]);
-                        
-                        if(!_threeConnected.Contains(tuple))
-                            _threeConnected.Add(tuple);
-                    
-                    }
-
-                       
-
+               list.AddRange(_keyValuePairs[el]);
             }
-
+            var grouped = list.GroupBy(o => o).Select(o=>new { Key = o.Key, Number = o.Count()}).ToList();
+            var max = grouped.Max(o=>o.Number);
+            while (max > 0)
+            {
+                var broj = grouped.Where(o => o.Number >= max).Count();
+                if (broj == max && broj > totalMax)
+                {
+                    var setList = grouped.Where(o => o.Number >= max);
+                    _wifi = String.Join(",", setList.Select(o=>o.Key).Order());
+                    totalMax = broj;
+                }
+                max--;
+            }
         }
-
-
-        Console.WriteLine(_threeConnected.Where(o => o.Item1.StartsWith("t") || o.Item2.StartsWith("t") || o.Item3.StartsWith("t")).Count());
+        Console.WriteLine(_wifi);
         Console.ReadLine();
-        
     }
-
-
-
-   
-
 }
